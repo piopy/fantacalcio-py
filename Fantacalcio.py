a1f46c80a2f3cd82b5b9855859213f94466280fc
Antonio Volgarino <burningham18@gmail.com>
2022-08-07 23:52:24 +0200

    eta beta
    Ancora più brutto di prima

--- Fantacalcio.py
+++ Fantacalcio.py
@@ -1,238 +1,278 @@
-
-'''
-urls:
-quotazioni:
-https://www.fantacalcio.it//Servizi/Excel.ashx?type=0&r=1&t=1614489243000
-https://www.fantacalcio.it/quotazioni-fantacalcio
-
-voti:
-https://www.fantacalcio.it/Servizi/Excel.ashx?type=2&r=1&t=1614489243000&s=2020-21
-https://www.fantacalcio.it/statistiche-serie-a/2020-21/fantacalcio/medie
-'''
-
-import warnings
-warnings.filterwarnings("ignore")
-import pandas as pd
+import ast
 import os
-import wget, requests
+from random import randint
+import time
+from bs4 import BeautifulSoup
+import requests
+from tqdm import tqdm
+import pandas as pd
+from loguru import logger
 
-# hanno cambiato struttura del sito quindi li scarico a mano e fine perchè non ho voglia di automatizzare il login. 
-# se qualcuno ha sta sbatta, il pulsante fork sta in alto a destra
+ruoli = ["Portieri", "Difensori", "Centrocampisti", "Trequartisti", "Attaccanti"]
 
-# def quotazioni():
-#     url='https://www.fantacalcio.it/quotazioni-fantacalcio'
-#     #www.fantacalcio.it//Servizi/
-#     req=requests.get(url).text
-#     req=[i for i in req.splitlines() if 'www.fantacalcio.it//Servizi/' in i][0]
-#     # req=req.replace('            location.href = "//','https://').replace('"','')
-#     req=req.replace('            location.href = "','').replace('"','')
-#     try:os.remove('./Quotazioni_Fantacalcio_Ruoli_Fantagazzetta2.xlsx')
-#     except: pass
-#     wget.download(req,out='./Quotazioni_Fantacalcio_Ruoli_Fantagazzetta2.xlsx')
-#     print('\n Download delle quotazioni completato\n')
-#     return True
-
-# def scarica(stagione):
-#     url='https://www.fantacalcio.it/statistiche-serie-a/'+stagione+'/fantacalcio/medie'
-#     #            location.href = "//www.fantacalcio.it/Servizi/
-#     req=requests.get(url).text
-#     req=[i for i in req.splitlines() if 'www.fantacalcio.it/Servizi/' in i and stagione in i][0]
-#     req=req.replace('            location.href = "','').replace('"','')
-#     try:os.remove('./Statistiche_Fantacalcio_'+stagione+'_Fantagazzetta.xlsx')
-#     except: pass
-#     wget.download(req,out='./Statistiche_Fantacalcio_'+stagione+'_Fantagazzetta.xlsx')
-#     print("\nDataset stagione {} completato.".format(stagione))
-#     return True
-
-def prune(dataset):
-	temp=pd.DataFrame()
-	temp['Nome']=dataset['Nome']
-	temp['Pg']=dataset['Pg']
-	temp['Mf']=dataset['Mf']
-	return temp
-	
-if __name__=='__main__':
-	manuale=False
-	scelta=input("Inserire stagione nel formato yyyy-yy (es. 2022-23): ")
-	if scelta=='': 
-		scelta='2022-23'
-
-	anno1=int((scelta.split('-'))[0])
-	scelta2=str(anno1-1)+'-'+str(anno1%2000)
-	scelta3=str(anno1-2)+'-'+str(anno1%2000-1)
-	# print("Scaricando le statistiche di ",scelta,scelta2,scelta3) # questo codice si fa sempre più brutto gente
-	scelta1=scelta #non ho sbatta di rinominare
-	# try:
-	# 	quotazioni()
-
-	# 	scarica(scelta1)
-	# 	scarica(scelta2)
-	# 	scarica(scelta3)
-	# except Exception as e: 
-	# 	print("\nErrore nei download\n",e)
-	# 	exit(0)
-
-	dataset_quotazioni = pd.read_excel("./Quotazioni_Fantacalcio_Ruoli_Fantagazzetta2.xlsx",header = 1,engine='openpyxl')
-
-	#debug 
-	list(dataset_quotazioni.columns.values)
-	#['Id', 'R', 'Nome', 'Squadra', 'Qt. A', 'Qt. I', 'Diff.']
-
-	#droppo le colonne che non voglio utilizzare nel dataset finale
-	dataset_quotazioni = dataset_quotazioni.drop('Id',1)
-	dataset_quotazioni = dataset_quotazioni.drop('Diff.',1)
-	#debug
-	#list(dataset_quotazioni.columns.values)
-	#['R', 'Nome', 'Squadra', 'Qt. A', 'Qt. I']
-
-	dataset_statistiche1 = pd.read_excel("./Statistiche_Fantacalcio_"+scelta3+"_Fantagazzetta.xlsx",header = 1,engine='openpyxl')
-	dataset_statistiche2 = pd.read_excel("./Statistiche_Fantacalcio_"+scelta2+"_Fantagazzetta.xlsx",header = 1,engine='openpyxl')
-	dataset_statistiche3 = pd.read_excel("./Statistiche_Fantacalcio_"+scelta1+"_Fantagazzetta.xlsx",header = 1,engine='openpyxl')
-
-	calciatorioggi=list(dataset_statistiche3['Nome']+'/'+dataset_statistiche3['Squadra'])
-	calciatoriold=list(dataset_statistiche2['Nome']+'/'+dataset_statistiche2['Squadra'])
-	calciatoriold_dict = dict(zip(dataset_statistiche2['Nome'].to_list(), dataset_statistiche2['Squadra'].to_list()))
-	giveatry=[i.split('/')[0] for i in calciatorioggi if i not in calciatoriold]
-
-	#debug 
-	list(dataset_statistiche1.columns.values)
-	#['Id', 'R', 'Nome', 'Squadra', 'Pg', 'Mv', 'Mf', 'Gf', 'Gs', 'Rp', 'Rc', 'R+', 'R-', 'Ass', 'Asf', 'Amm', 'Esp', 'Au']
-	#nome, partite_giocate,media_fantacalcio
-	#['Nome', 'Pg', 'Mf']
-	dataset_statistiche1=prune(dataset_statistiche1)
-	dataset_statistiche2=prune(dataset_statistiche2)
-	dataset_statistiche3=prune(dataset_statistiche3)
-
-	df1 =pd.DataFrame(dataset_quotazioni)
-	df2 =pd.DataFrame(dataset_statistiche1)
-	df3 =pd.DataFrame(dataset_statistiche2)
-	df4 =pd.DataFrame(dataset_statistiche3)
-
-	dataset = df1.merge(df2,on="Nome").merge(df3,on="Nome").merge(df4,on="Nome")
-	#result = dataset.sort_values(by='Qt. I')
-
-################# core 
-	media_giocatori = [];
-	for index, row in dataset.iterrows():
-		if  row.Pg_x > 0 or row.Pg_y > 0:	
-			media_pesata_partite_giocate = (row.Pg_x/38 * row.Mf_x)*0.20 + (row.Pg_y/38 * row.Mf_y)*0.80 
-			media_giocatori.append(media_pesata_partite_giocate)
-		else:
-			media_giocatori.append(0)
-
-	dataset["mediaGiocatori"] = media_giocatori
-	media = []
-	
-	for index, row in dataset.iterrows():
-		if  row.mediaGiocatori > 0:	 
-			media.append(row.mediaGiocatori/row["Qt.I"])
-		else:
-			media.append(0)
-	dataset["media"] = media
-
-	giocatemax=0
-	for index, row in dataset.iterrows():
-			if  row.Pg > giocatemax: giocatemax=row.Pg
-		
-	print("in questa stagione sono state disputate ",giocatemax," partite")
-
-	if giocatemax==0:giocatemax=1 #se il campionato non è iniziato evito la divisione per zero
-
-	media_giocatori = [];
-	for index, row in dataset.iterrows():
-		if  row.Pg > 0:	 
-			media_pesata_partite_giocate = (row.Pg_x/38 * row.Mf)*0.20 + (row.Pg_y/38 * row.Mf)*0.40 + (row.Pg/giocatemax * row.Mf)*0.40
-			media_giocatori.append(media_pesata_partite_giocate)
-		else:
-			media_giocatori.append(0)
-
-	dataset["mediaGiocatori_today"] = media_giocatori
-
-	media = []
-
-	for index, row in dataset.iterrows():
-		if  row.mediaGiocatori_today > 0:	 
-			media.append(row.mediaGiocatori/row["Qt.I"])
-		else:
-			media.append(0)
-	dataset["media_today"] = media
+skills = {
+    "Fuoriclasse": 2,
+    "Titolare": 2,
+    "Buona Media": 2,
+    "Goleador": 3,
+    "Assistman": 2,
+    "Piazzati": 2,
+    "Rigorista": 3,
+    "Giovane talento": 2,
+    "Panchinaro": -2,
+    "Falloso": -3,
+    "Outsider": 2,
+}
 
 
-	prob=[]
-	for index, row in dataset.iterrows():
-		if int(row.Pg_y) <= int(row.Pg_x):	 ## rivedere calcolo
-			if round(int(row.Pg_y)/38*100-(int(row.Pg_x+1)/int(row.Pg_y+1))) < 0 : prob.append(round(-1/2*(int(row.Pg_y)/38*100-(int(row.Pg_x+1)/int(row.Pg_y+1)))))
-			else : prob.append(round(int(row.Pg_y)/38*100-(int(row.Pg_x+1)/int(row.Pg_y+1))))#/38*100))
-		else:
-			if round(int(row.Pg_y)/38*100+(int(row.Pg_y)/int(row.Pg_x+1))) > 100: prob.append(99.5)
-			else : prob.append(round(int(row.Pg_y)/38*100+(int(row.Pg_y)/int(row.Pg_x+1))))#/38*100)/2)
+def get_giocatori(ruolo: str) -> list:
 
-	dataset["Probabile titolarità futura (occhio se il giocatore ha cambiato squadra)"] = prob
+    html = requests.get(
+        "https://www.fantacalciopedia.com/lista-calciatori-serie-a/"
+        + ruolo.lower()
+        + "/"
+    )
+    soup = BeautifulSoup(html.content, "html.parser")
+    calciatori = []
+    giocatori = soup.find_all("article")
+    for giocatore in giocatori:
+        calciatore = giocatore.find("a").get("href")
+        calciatori.append(calciatore)
 
-	prob=[]
-	for index, row in dataset.iterrows():
-		if row.Nome in giveatry:	 
-			prob.append('Y - ex '+calciatoriold_dict[row.Nome])
-		else:
-			prob.append('N')
-
-	dataset["Squadra Nuova"] = prob
-
-	total = []
-	for index, row in dataset.iterrows():
-		if  row.mediaGiocatori > 0:	 
-			total.append(row.mediaGiocatori*row.media*row['Mf_y'])
-		else:
-			total.append(0)
-
-	dataset["convenienza_inizio_campionato (considera solo le due annate precedenti concluse)"] = total
+    return calciatori
 
 
-	total = []
-	for index, row in dataset.iterrows():
-		if  row.mediaGiocatori > 0:	 
-			total.append(row.mediaGiocatori_today*row.media_today*row['Mf'])
-		else:
-			total.append(0)
+def get_attributi(url: str) -> dict:
+    time.sleep(randint(0, 2000) / 1000)
+    attributi = dict()
+    html = requests.get(url.strip())
+    soup = BeautifulSoup(html.content, "html.parser")
+    attributi["Nome"] = soup.select_one("h1").get_text().strip()
 
-	dataset["convenienza_today"] = total
+    selettore = "div.col_one_fourth:nth-of-type(1) span.stickdan"
+    attributi["Punteggio"] = soup.select_one(selettore).text.strip().replace("/100", "")
 
-	total = []
-	for index, row in dataset.iterrows():
-		if  row.convenienza_today > row.mediaGiocatori*row.media*row['Mf_y']:	 
-			total.append("U")
-		else:
-			total.append("D")
-	dataset["Up/Down"] = total 
+    selettore = "	div.col_one_fourth:nth-of-type(n+2) div"
+    medie = [el.find("span").text.strip() for el in soup.select(selettore)]
+    anni = [
+        el.find("strong").text.split(" ")[-1].strip() for el in soup.select(selettore)
+    ]
+    i = 0
+    for anno in anni:
+        attributi[f"Fantamedia anno {anno}"] = medie[i]
+        i += 1
 
-	if giocatemax < 2: result = dataset.sort_values(by="convenienza_inizio_campionato (considera solo le due annate precedenti concluse)",ascending=False)
-	else: result = dataset.sort_values(by="convenienza_today",ascending=False)
-	
-	#drop mediaGiocatori	media	mediaGiocatori_today	media_today
+    selettore = "div.col_one_third:nth-of-type(2) div"
+    stats_ultimo_anno = soup.select_one(selettore)
+    parametri = [
+        el.text.strip().replace(":", "") for el in stats_ultimo_anno.find_all("strong")
+    ]
+    valori = [el.text.strip() for el in stats_ultimo_anno.find_all("span")]
+    attributi.update(dict(zip(parametri, valori)))
 
-	result = result.drop('mediaGiocatori',1) 
-	result = result.drop('media',1) 
-	result = result.drop('mediaGiocatori_today',1) 
-	result = result.drop('media_today',1) 
+    selettore = ".col_one_third.col_last div"
+    stats_previste = soup.select_one(selettore)
+    parametri = [
+        el.text.strip().replace(":", "") for el in stats_previste.find_all("strong")
+    ]
+    valori = [el.text.strip() for el in stats_previste.find_all("span")]
+    attributi.update(dict(zip(parametri, valori)))
+
+    selettore = ".label12 span.label"
+    ruolo = soup.select_one(selettore)
+    attributi["Ruolo"] = ruolo.get_text().strip()
+
+    selettore = "span.stickdanpic"
+    skills = [el.text for el in soup.select(selettore)]
+    attributi["Skills"] = skills
+
+    selettore = "div.progress-percent"
+    investimento = soup.select(selettore)[2]
+    attributi["Buon investimento"] = investimento.text.replace("%", "")
+
+    selettore = "div.progress-percent"
+    investimento = soup.select(selettore)[3]
+    attributi["Resistenza infortuni"] = investimento.text.replace("%", "")
+
+    selettore = "img.inf_calc"
+    try:
+        consigliato = soup.select_one(selettore).get("title")
+        if "Consigliato per la giornata" in consigliato:
+            attributi["Consigliato prossima giornata"] = True
+        else:
+            attributi["Consigliato prossima giornata"] = False
+
+    except:
+        attributi["Consigliato prossima giornata"] = False
+
+    selettore = "span.new_calc"
+    nuovo = soup.select_one(selettore)
+    if not nuovo == None:
+        attributi["Nuovo acquisto"] = True
+    else:
+        attributi["Nuovo acquisto"] = False
+
+    selettore = "img.inf_calc"
+    try:
+        infortunato = soup.select_one(selettore).get("title")
+        if "Infortunato" in infortunato:
+            attributi["Infortunato"] = True
+        else:
+            attributi["Infortunato"] = False
+
+    except:
+        attributi["Infortunato"] = False
+
+    selettore = "#content > div > div.section.nobg.nomargin > div > div > div:nth-child(2) > div.col_three_fifth > div.promo.promo-border.promo-light.row > div:nth-child(3) > div:nth-child(1) > div > img"
+    squadra = soup.select_one(selettore).get("title").split(":")[1].strip()
+    attributi["Squadra"] = squadra
+
+    selettore = "	div.col_one_fourth:nth-of-type(n+2) div"
+    try:
+        trend = soup.select(selettore)[0].find("i").get("class")[1]
+        if trend == "icon-arrow-up":
+            attributi["Trend"] = "UP"
+        else:
+            attributi["Trend"] = "DOWN"
+    except:
+        attributi["Trend"] = "STABLE"
+
+    selettore = "div.col_one_fourth:nth-of-type(2) span.rouge"
+    presenze_attuali = soup.select_one(selettore).text
+    attributi["Presenze campionato corrente"] = presenze_attuali
+
+    return attributi
 
 
+def appetibilita(df: pd.DataFrame) -> float:
+    # cleaning
+    for col in df.columns:
+        df.loc[df[col] == "nd", col] = 0
 
-########### generating output ##################
+    # appetibilità =( Fantamedia anno scorso * Partite giocate anno scorso/38 * peso
+    #              + Fantamedia anno corrente * Partite giocate anno corrente/giornata * 100-peso )/ quotazione
+    #              + skills + altri parametri
+    res = []
+    giocatemax = 1
 
-	try:os.mkdir("./res")
-	except:pass
-	print("Generating xls...")
-	result.to_excel('./res/result.xls')
+    for index, row in df.iterrows():
+        if int(row[-1]) > giocatemax:
+            giocatemax = row[-1]
 
-	df = result[result["R"] == "A"]
-	df.to_excel('./res/dataset_A_.xls')
+    for index, row in df.iterrows():
+        appetibilita = 0
+        appetibilita_today = 0
 
-	df = result[result["R"] == "C"]
-	df.to_excel('./res/dataset_C_.xls')
+        # media pesata fantamedia
+        if int(row[5]) > 0:
+            appetibilita += float(row[7]) * int(row[5]) / 38   *20/100 #era row 2
+        
+        if not (
+            df.columns[2].split(" ")[-1] == df.columns[6].split(" ")[-1]
+            and int(row[-1]) > 5
+        ):  # stesso anno
+            appetibilita = (
+                appetibilita * float(row[6]) * int(row[-1]) / giocatemax *80/100
+            )  
+        else: 
+            appetibilita = float(row[7]) * int(row[5]) / 38 
 
-	df = result[result["R"] == "D"]
-	df.to_excel('./res/dataset_D_.xls')
+        # media pesata fantamedia * convenienza rispetto alla quotazione * media scorso anno
+        if float(row[1]) == 0: pt=1
+        else: pt=float(row[1])
+        appetibilita = (
+            # appetibilita * appetibilita / pt * 100 / 40 * float(row[2])
+             appetibilita / pt * 100 / 40 
+        ) 
 
-	df = result[result["R"] == "P"]
-	df.to_excel('./res/dataset_P_.xls')
-	input("Premere qualsiasi tasto per terminare")
+        # skills
+        try:
+            valori = ast.literal_eval(row[-9])
+            plus = 0
+            for skill in valori:
+                plus += skills[skill]
+            appetibilita += plus
+        except:
+            pass
+
+        if row["Nuovo acquisto"]:
+            appetibilita -= 2
+        if row["Buon investimento"] == 60:
+            appetibilita += 3
+        if row["Consigliato prossima giornata"]:
+            appetibilita += 1
+        if row["Trend"] == "UP":
+            appetibilita += 2
+        if row["Infortunato"]:
+            appetibilita -= 1
+        if row["Resistenza infortuni"] > 60:
+            appetibilita += 4
+
+        res.append(appetibilita)
+
+    return res
+
+
+if __name__ == "__main__":
+    giocatori_urls = []
+    if not os.path.exists("giocatori_urls.txt"):
+        for i in tqdm(range(0, len(ruoli), 1)):
+            lista = get_giocatori(ruoli[i])
+            [giocatori_urls.append(el) for el in lista]
+        with open(r"giocatori_urls.txt", "w") as fp:
+            for item in giocatori_urls:
+                fp.write("%s\n" % item)
+            logger.debug("URL scritti")
+    else:
+        logger.debug("Leggo la lista giocatori")
+        with open("giocatori_urls.txt", "r") as fp:
+            giocatori_urls = fp.readlines()
+
+    if not os.path.exists("giocatori.csv"):
+        giocatori = []
+        for i in tqdm(range(0, len(giocatori_urls), 1)):
+            giocatore = get_attributi(giocatori_urls[i])
+            giocatori.append(giocatore)
+        df = pd.DataFrame.from_dict(giocatori)
+        df.to_csv("giocatori.csv", index=False)
+        
+        logger.debug("CSV scritto")
+    else:
+        logger.debug("Leggo il dataset giocatori")
+        df = pd.read_csv("giocatori.csv")
+
+    df["Convenienza"] = appetibilita(df)
+
+    # riordino le colonne come piace a me
+    temp = df.columns
+    df = df[
+        [
+            temp[11],
+            temp[0],
+            temp[18],
+            temp[1],
+            temp[20],
+            temp[21],
+            temp[19],
+            temp[6],
+            temp[2],
+            temp[5],
+            temp[7],
+            temp[3],
+            temp[4],
+            temp[16],
+            temp[17],
+            temp[8],
+            temp[9],
+            temp[10],
+            temp[13],
+            temp[14],
+            temp[12],
+            temp[15],
+        ]
+    ]
+    df.sort_values(by="Convenienza", ascending=False)
+
+    # df.to_csv("giocatori_appet.csv", index=False)
+    df.to_excel("giocatori_excel2.xls")
+    logger.debug("Finito!")
--- README.md
+++ README.md
@@ -1,21 +1,20 @@
-
+# Versione 2 -  Ancora più brutto
 
 # COSA E' FANTACALCIO-PY?
- Un tool che scarica i database più recenti degli ultimi due campionati, il database del campionato attuale e le quotazioni di quest'ultimo.
- Il programma provvederà poi a incrociare i dati mostrando l'andamento di un giocatore, la sua media negli ultimi due campionati (rispettivamente indicati con x e y nell'excel di output) e in quello attuale, la sua probabile titolarità futura (dato interessante se siamo ad inizio campionato) e due indici, detti di convenienza.
- Tutti questi dati verranno poi messi in vari Excel (Attaccanti Centrocampisti Difensori Portieri e result che è quello generale) per essere comparati.
+ Un tool che scarica i database più recenti degli ultimi due campionati.
+ Il programma provvederà poi a incrociare i dati mostrando l'andamento di un giocatore, la sua media negli ultimi due campionati la sua probabile titolarità futura (dato interessante se siamo ad inizio campionato) e due indici, uno di convenienza estrapolato dai dati e uno preso dal sito fantacalciopedia.
+ Tutti questi dati verranno poi messi in un Excel per essere comparati.
 
 # COSA INDICA LA CONVENIENZA MO?
  Per convenienza si intende il rapporto tra il valore di base in asta e il suo rendimento passato o attuale (indicato con "convenienza today")
- Ad esempio Lukaku è una macchina da gol ma la sua convenienza sarà più bassa rispetto a Muriel, che ha non solo una media simile, ma un prezzo base inferiore.
- Allo stesso tempo, pur essendo a parità di prezzo base, Lukaku sarà più conveniente di Ronaldo per via delle partite giocate e del rendimento.
- Ovviamente questo tool è da usare leggendo l'output dato dallo stesso programma, poichè è stato ideato non per prendere i Lukaku, bensì per fare colpi a basso prezzo di giocatori ovviamente non blasonati ma comunque utili alla causa (es. un Lykogiannis o uno Svanberg presi a poco che si son dimostrati degni del ruolo di riserva).
- Può capitare di prendere giocatori come Lukic che vi faranno rimpiangere di aver usato il mio algoritmo per guidarvi all'asta, ma vabbè mo lo metto come disclaimer.
+ Ad esempio Immobile è una macchina da gol ma la sua convenienza sarà più bassa rispetto a Vlahovic, che ha non solo una media simile, ma un prezzo base inferiore.
+ Allo stesso tempo, pur essendo a parità di prezzo base, un giocatore come Luis Alberto sarà più conveniente di Barak per via delle partite giocate e del rendimento.
+ Ovviamente questo tool è da usare leggendo l'output dato dallo stesso programma, poichè è stato ideato non per prendere giocatori come Immobile, bensì per fare colpi a basso prezzo di giocatori ovviamente non blasonati ma comunque utili alla causa (es. un Deulofeu o uno Arnautovic presi a poco che si son dimostrati degni del ruolo di riserva).
+ Può capitare di prendere giocatori come Verde che vi faranno rimpiangere di aver usato il mio algoritmo per guidarvi all'asta, ma vabbè mo lo metto come disclaimer.
 
  # DISCLAIMER
- - Se perdete il fanta non è colpa mia, io ci so arrivato secondo co sta roba.
- - Servono pandas e wget per funzionare
- - Il tool utilizza i csv prodotti da Fantagazzetta, tutti i dati processati sono loro, anche la quotazione di Muriel che è più bassa di quella di Ronaldo. Io boh.
+ - Se perdete il fanta non è colpa mia, io ci so arrivato secondo co sta roba. E l'anno dopo primo.
+ - Servono pandas e bs4 per funzionare
+ - Il tool utilizza i csv prodotti da fantacalciopedia, tutti i dati processati sono loro, dato che fantagazzetta ha deciso di tagliare i dataset open
 
- ### SPECIALI RINGRAZIAMENTI
- a cttynul per l'idea base 
+ 
