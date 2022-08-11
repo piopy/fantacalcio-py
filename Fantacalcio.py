0fc883593285ef8c86a704ea17f552cec53f42a0
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-08-11 12:35:13 +0200

    aggiunto casting in alcune assegnazioni
    in alcune assegnazioni veniva effettuato il confronto tra una str e un numero intero, oppure si tentava di moltiplicare un str con un float (tutte le str comunque rappresentavano numeri)

--- Fantacalcio.py
+++ Fantacalcio.py
@@ -275,6 +275,6 @@ if __name__ == "__main__":
     ]
     df.sort_values(by="Convenienza", ascending=False)
 
-    # df.to_csv("giocatori_appet.csv", index=False)
+    #df.to_csv("giocatori_appet.csv", index=False)
     df.to_excel("giocatori_excel.xls")
     logger.debug("Finito!")
